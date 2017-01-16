c2b8d27853af234d42dbfd12d5defdb4442d2ff2
Kevin Krummenauer <kevin.krummenauer@adesso.de>
2017-01-16 16:38:20 +0100

    erste REST Endpoint für generierung von Testdaten und Abfragen von Kommentaren

--- main.js
+++ main.js
@@ -1,34 +1,67 @@
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
+var mongoClient = require('mongodb').MongoClient;
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var mongoHost = "10.1.6.210";
+var mongoPort = "27017";
+var mongoDB;
+const COMMENTS_COLLECTION = "comments";
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+mongoClient.connect("mongodb://"+mongoHost+":"+mongoPort+"/test", function(err, db) {
+    if(err) {
+        console.log("No connection to Mongo " + mongoHost + ":" + mongoPort);
+        process.exit(1);
+    }
+
+    mongoDB = db;
+    console.log("Connected to Mongo DB " + mongoHost + ":" + mongoPort);
 });
 
-// more routes for our API will happen here
+var router = express.Router();
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+router.get('/comments/:newsId', function(req, res) {
+    var newsId = req.params.newsId;
+
+    console.log(newsId);
+    mongoDB.collection(COMMENTS_COLLECTION).find().toArray(function(err, comments) {
+        res.json(comments);
+    });
+
+});
+
+
+router.get('/createTestdata', function(req, res) {
+
+    console.log("Create Testdata");
+    var collection = mongoDB.collection(COMMENTS_COLLECTION);
+    collection.drop();
+    collection = mongoDB.collection(COMMENTS_COLLECTION);
+
+    collection.insert({
+        "newsid": "news-schnee",
+        "text": "Hallo Welt",
+        "autor": "Maxi"
+    });
+
+    collection.insert({
+        "newsid": "news-schnee",
+        "text": "Ab auf die Piste!",
+        "autor": "Maxi Mera"
+    });
+
+    res.json({ message: 'ok' });
+});
+
+
+
 app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Server started on port ' + port);
