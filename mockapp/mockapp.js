a891db1900fb2b17dff1f6aa0e9d400b3f49df3f
Kevin Krummenauer <kevin.krummenauer@adesso.de>
2017-02-01 18:41:38 +0100

    mockapp entfernt

--- mockapp/mockapp.js
+++
@@ -1,6 +0,0 @@
-angular.module('NewsApp', [])
-    .controller('MockCtrl', function ($scope, $http) {
-
-        $scope.commentsservice = {};
-        $scope.commentsservice.newsId = 'news-schnee';
-    });
